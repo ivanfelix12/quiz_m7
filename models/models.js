f991669f3282c132aeac18d92716ff150e9a0c43
Ivan Felix Lopez <ivanfelix12@gmail.com>
2015-07-17 03:43:11 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -37,9 +37,10 @@ sequelize.sync().success(function(){
 	Quiz.count().success(function(count){
 		if(count === 0) {	// la tabla se inicializa solo si esta vacia
 			Quiz.create({	pregunta: 'Capital de Italia',
-							respuesta: 'Roma'}).success(function() {
-								console.log('Base de datos inicializada')
-							});
+							respuesta: 'Roma'});
+			Quiz.create({	pregunta: 'Capital de Portugal',
+							respuesta: 'Lisboa'})
+			.then(function(){conslole.log('Base de datos inicializada')});
 		};
 	});
 });
