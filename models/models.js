ac691c6c995de14d399b55086eefc6ffb0f97a36
Ivan Felix Lopez <ivanfelix12@gmail.com>
2015-08-04 00:17:46 +0200

    Ejercicio modulo 8 (1)

--- models/models.js
+++ models/models.js
@@ -32,9 +32,9 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
 	//success(...) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+	Quiz.count().then(function(count){
 		if(count === 0) {	// la tabla se inicializa solo si esta vacia
 			Quiz.create({	pregunta: 'Capital de Italia',
 							respuesta: 'Roma',
