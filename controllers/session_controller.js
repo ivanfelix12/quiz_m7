3028c0e4cbd2ccdbe3f642bd5a72cbcdae7057b4
Ivan Felix <ivanfelix12@gmail.com>
2015-08-06 19:13:34 +0200

    Autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+//MiddleWare de autorizacion de accesos HTTP restringidos
+exports.loginRequired = function(req,res,next){
+	if(req.session.user){
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
 //GET /login -- Fromulario de login
 exports.new = function(req,res){
 	var errors = req.session.errors || {};
